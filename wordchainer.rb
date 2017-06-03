1caacba56eb07c24260994a6ab081ab066b37c61
Norris Kwan <norris.kwan@gmail.com>
2017-06-03 00:12:18 -0700

    Add #run

--- wordchainer.rb
+++ wordchainer.rb
@@ -10,6 +10,26 @@ class WordChainer
     @dictionary = Set.new(File.readlines(dictionary_file_name).map(&:chomp))
   end
 
+  def run(source, target)
+    @current_words = [source]
+    @all_seen_words = { source => nil }
+
+    until @current_words.empty?
+      new_current_words = []
+
+      @current_words.each do |current_word|
+        adjacent_words(current_word).each do |adjacent_word|
+          next if @all_seen_words.include? target
+          @all_seen_words[adjacent_word] = current_word
+          new_current_words << adjacent_word
+        end
+      end
+      print new_current_words
+      @current_words = new_current_words
+    end
+    p "@all_seen_words: #{@all_seen_words}"
+  end
+
   def adjacent_words(word)
     adjacent_words = []
     alphabet = ('a'..'z')
@@ -24,7 +44,7 @@ class WordChainer
         adjacent_words << test_word if @dictionary.include? test_word
       end
     end
-    p adjacent_words
+    adjacent_words
   end
 
 end
@@ -32,5 +52,5 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   start = WordChainer.new('dictionary.txt')
-  start.adjacent_words('cat')
+  start.run("duck", "ruby")
 end
