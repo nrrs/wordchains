e0aaec7f6f3a8c54d42932bc4a036ea0cbd928c5
Norris Kwan <norris.kwan@gmail.com>
2017-06-02 23:45:07 -0700

    Add dictionary.txt

--- wordchainer.rb
+++ wordchainer.rb
@@ -10,13 +10,11 @@ class WordChainer
     @dictonary = Set.new(File.readlines(dictionary_file_name).map(&:chomp))
   end
 
-  def adjacent_words(word)
 
-  end
 
 end
 
 
 if __FILE__ == $PROGRAM_NAME
-  
+  start = WordChainer.new('dictionary.txt')
 end
